cea4f98e8e55ca22da3ad7db233da20499ae9b24
Pavan Ingalagi <ingalagipavan@gmail.com>
2022-12-15 22:07:18 +0530

    product page

---
+++ scripts/productPage.js
@@ -0,0 +1,47 @@
+let products = [
+    {
+        image:`https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw68829434/crop/026587279_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_`,
+        title: "Black Tie",
+        price: 7.50,
+        rating: 4.5,
+        type: "wallflowers Fragrance Refill",
+
+    },
+    {
+        image:`https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwc1518184/crop/026438827_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_`,
+        title: "Black Tie",
+        desc: "Wallflowers Fragrance Refill",
+        price: 7.00,
+        rating: 4.0,
+        type: "wallflowers Fragrance Refill",
+    },
+    {
+        image:`https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwdfe0df3d/crop/026437376_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_`,
+        title: "Mahogany Teakwood",
+        price: 15.50,
+        rating: 4.5,
+        type: "Single Wick Candle",
+    },
+    {
+        image:`https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw788dbf70/crop/026501180_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_`,
+        title: "Birch & Ecalyptus",
+        price: 16.50,
+        rating: 4.5,
+        type: "Unlimited Hydration Body Cream",
+    },
+    {
+        image:`https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwe8387708/crop/026534930_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_`,
+        title: "Flannel",
+        price: 8.50,
+        rating: 4.5,
+        type: "Cocentrated Room Spray",
+    },
+]
+
+const renderDom =  () => {
+
+// console.log('slider');
+let container = document.getElementById("productContainer");
+    console.log(container);
+}; 
+renderDom();
