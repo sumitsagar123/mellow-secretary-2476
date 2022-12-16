832a5fad5476358fbcf6ac07c254a794bca12b6e
Pavan Ingalagi <ingalagipavan@gmail.com>
2022-12-16 23:04:32 +0530

    product page

--- scripts/productPage.js
+++ scripts/productPage.js
@@ -36,12 +36,106 @@ let products = [
         rating: 4.5,
         type: "Cocentrated Room Spray",
     },
+    {
+        image:`https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw4a25f053/crop/026584954_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_`,
+        title: "Flannel",
+        price: 16.95,
+        rating: 4.5,
+        type: "3-Wick Candle",
+    },
+    {
+        image:`https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwef08bb2c/crop/026552804_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_`,
+        title: "Bpurbon",
+        price: 16.50,
+        rating: 5.0,
+        type: "Ultimate Hydration Body Cream",
+    },
+    {
+        image:`https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwcff163db/crop/026358678_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_`,
+        title: "Mahogany Teakwood Increased Intensity",
+        price: 16.50,
+        rating: 5.0,
+        type: "Wallflowers Fragrance Refill",
+    },
+    {
+        image:`https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw8c66aeff/crop/026552809_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_`,
+        title: "Whiskey Reserve",
+        price: 14.50,
+        rating: 4.6,
+        type: "Daily Nourishing Body Lotion",
+    },
+    {
+        image:`https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw41ccd312/crop/026438828_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_`,
+        title: "Vintage",
+        price: 7.50,
+        rating: 4.1,
+        type: "Gentle Foaming Hand Soap",
+    },
+    {
+        image:`https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwdfe3b194/crop/026551686_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_`,
+        title: "Smoked Old Fashioned",
+        price: 14.50,
+        rating: 4.0,
+        type: "Body Spray",
+    },
+    {
+        image:`https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwd11e9755/crop/026466941_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_`,
+        title: "Freshwater",
+        price: 7.50,
+        rating: 4.5,
+        type: "Wallflowers Fragrance Refill",
+    },
+    {
+        image:`https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwc05d9d02/crop/026551691_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_`,
+        title: "Whiskey Reserve",
+        price: 15.50,
+        rating: 4.8,
+        type: "3-in-1 hair, Face & Body Wash",
+    },
 ]
 
 const renderDom =  () => {
 
 // console.log('slider');
 let container = document.getElementById("productContainer");
-    console.log(container);
+let data =JSON.parse(localStorage.getItem("Productcart")) || [];
+    // console.log(container);
+    products.forEach((el) => {
+
+        let div = document.createElement("div");
+            div.setAttribute("class", "productss");
+
+        let img = document.createElement("img");
+            img.src = el.image;
+
+        let title = document.createElement("h3");
+        let titleNode = document.createTextNode(el.title);
+            title.appendChild(titleNode);
+
+        let type = document.createElement("p");
+        let typeNode = document.createTextNode(el.type);
+            type.appendChild(typeNode);
+
+        let price = document.createElement("h3");
+        let priceNode = document.createTextNode(`$ ${el.price}`);
+            price.appendChild(priceNode);
+   
+        let rating = document.createElement("p");
+        let ratingNode = document.createTextNode(`Rating: ${el.rating}`);
+            rating.appendChild(ratingNode);
+
+            let button = document.createElement("button");
+                button.innerText= "Add to Cart";
+                button.onclick = () => {
+                    // addToCart(el);
+                    data.push(el);
+                    localStorage.setItem("Productcart", JSON.stringify(data));
+                    console.log(data);
+                };
+
+        div.append(img,title,type,price,rating,button);
+        container.append(div);
+    });
+    
 }; 
 renderDom();
