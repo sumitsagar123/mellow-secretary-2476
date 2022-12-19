6fdb6cb59386e6235711a338034bf68c765684a4
mevicky9254 <vikkichoudhary6528@gmail.com>
2022-12-19 14:28:37 +0530

    all set

--- home.js
+++
@@ -1,7 +0,0 @@
-import { navbar , footer } from "./navbar.js";
-
-let nav_div= document.getElementById("navcontainer");
-nav_div.innerHTML=navbar();
-
-let foot_div=document.getElementById("footcontainer");
-foot_div.innerHTML=footer();
